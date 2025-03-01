dc1954ee7cde2e00f0f21f34fedfe36c3cc0b2be
Joel <joelsam388@gmail.com>
2025-03-01 13:34:44 +0100

    Modificacion columnas y filas

--- app/src/main/java/com/uni/miniact2/MainActivity.kt
+++ app/src/main/java/com/uni/miniact2/MainActivity.kt
@@ -4,7 +4,11 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
@@ -33,30 +37,30 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun MyApp(modifier: Modifier = Modifier){
-    Surface(
-        modifier = Modifier,
-        color = MaterialTheme.colorScheme.background
-    ){
-        Greeting("Android")
+fun MyApp(modifier: Modifier = Modifier, names: List<String> = listOf("Android", "Compose")){
+    Column(modifier = modifier.padding(vertical = 4.dp)){
+        for (name in names){
+            Greeting(name = name)
+        }
     }
 }
 
 @Composable
 fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Surface(color = MaterialTheme.colorScheme.primary){
-        Text(
-            text = "Hello $name!",
-            modifier = modifier.padding(20.dp)
-        )
+    Surface(color = MaterialTheme.colorScheme.primary, modifier = modifier.padding(vertical = 4.dp, horizontal = 8.dp)){
+        Column( modifier = modifier.padding(20.dp)){
+            Text(text = "Hello ")
+            Text(text = name)
+        }
+
     }
 
 }
 
-@Preview(showBackground = true, name = "Text preview")
+@Preview(showBackground = true, widthDp = 320)
 @Composable
 fun GreetingPreview() {
     MiniAct2Theme {
-        Greeting("Android")
+        MyApp()
     }
 }
