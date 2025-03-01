b360974353dffbf19590fb27e319b49248bc3d93
Joel <joelsam388@gmail.com>
2025-03-01 14:15:53 +0100

    Creación Pantallas

---
+++ app2/src/main/java/com/uni/app2/WaterCount.kt
@@ -0,0 +1,17 @@
+package com.uni.app2
+
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+
+@Composable
+fun WaterCount(modifier: Modifier = Modifier) {
+    val count = 0
+
+    Text(
+        text = "You've had $count glasses.",
+        modifier = modifier.padding(15.dp)
+    )
+}
---
+++ app2/src/main/java/com/uni/app2/WellnessScreen.kt
@@ -0,0 +1,9 @@
+package com.uni.app2
+
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+
+@Composable
+fun WellnessScreen(modifier: Modifier = Modifier) {
+    WaterCount(modifier)
+}
