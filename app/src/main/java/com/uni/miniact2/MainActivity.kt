f123ec061ac33e09ee54b64285b44c825c1ec074
Joel <joelsam388@gmail.com>
2025-03-01 13:47:50 +0100

    Modificacion botones

--- app/src/main/java/com/uni/miniact2/MainActivity.kt
+++ app/src/main/java/com/uni/miniact2/MainActivity.kt
@@ -10,12 +10,17 @@ import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.ElevatedButton
+import androidx.compose.material3.FilledIconButton
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.uni.miniact2.ui.theme.MiniAct2Theme
@@ -48,9 +53,20 @@ fun MyApp(modifier: Modifier = Modifier, names: List<String> = listOf("Android",
 @Composable
 fun Greeting(name: String, modifier: Modifier = Modifier) {
     Surface(color = MaterialTheme.colorScheme.primary, modifier = modifier.padding(vertical = 4.dp, horizontal = 8.dp)){
-        Column( modifier = modifier.padding(20.dp)){
-            Text(text = "Hello ")
-            Text(text = name)
+        Row(modifier = Modifier.padding(24.dp)){
+            Column( modifier = modifier.weight(1f)){
+                Text(text = "Hello ")
+                Text(text = name)
+            }
+            ElevatedButton(
+                onClick = {},
+                colors = ButtonDefaults.buttonColors(
+                    containerColor = Color.Yellow,
+                    contentColor = Color.Black
+                )
+            ) {
+                Text("Show More")
+            }
         }
 
     }
