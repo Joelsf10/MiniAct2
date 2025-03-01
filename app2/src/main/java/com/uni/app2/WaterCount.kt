5325717659a58965cde658cf78c1171bf35d6998
Joel <joelsam388@gmail.com>
2025-03-01 14:35:14 +0100

    Agregar funciones de memoria

--- app2/src/main/java/com/uni/app2/WaterCount.kt
+++ app2/src/main/java/com/uni/app2/WaterCount.kt
@@ -5,15 +5,19 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Button
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
 
 @Composable
 fun WaterCount(modifier: Modifier = Modifier) {
     Column ( modifier = modifier.padding(20.dp)){
-        var count = 0
+        var count by remember{mutableStateOf(0)}
 
-        Text("You've had $count glasses.")
+        Text("You've had ${count++} glasses.")
         Button(onClick = {count++}, Modifier.padding(top = 5.dp)) {
             Text("Add more")
         }
